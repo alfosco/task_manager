77bc71414f50c63db0234b89efc22f55ae662877
Alex Fosco <alex.hf88@gmail.com>
2017-01-23 17:49:54 -0700

    compeletes second part of tutorial; answers assigned questions

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,8 @@
 require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
-  set :root, File.expand_path("..", __dir__)
+  set :root, File.expand_path("..", __dir__) #ids base level of application; sets up a path to views directory
+  set :method_override, true # this allows us to use _method in the form
 
   get '/' do
     erb :dashboard
@@ -26,4 +27,32 @@ class TaskManagerApp < Sinatra::Base
     @task = Task.find(params[:id])
     erb :show
   end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/task/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
 end
+
+# task_manager_questions
+
+# 1. Define CRUD.
+# -CRUD is an acronym that stands for Create, Read, Update, Delete.
+# 2. Why do we use set method_override: true?
+# -HTML does not allow the use of <em>method='put'</em> in a form tag. By using the given code snippet you can pass it as a hidden value which gives our controller the information it needs to route the request correctly.
+# 3. Explain the difference between value and name in this line: <input type='text' name='task[title]' value="<%= @task.title %>"/>.
+# -Value is the result of the instance variable <em>task</em> with the method <em>title</em> called on it. Name looks like it's equal to a hash named <em>task</em>, and task is returning the value that <em>title</em> points to.
+# 4. What are params? Where do they come from?
+# -Params is a hash that is storing our tasks.
+# 5. Check out your routes. Why do we need two routes each for creating a new Task and editing an existing Task?
+# -They each need a route to the controller for the link that someone selects, and a route to the  corresponding view that will be rendered when they hit the new route.
